ad3df8ff82fae2e20cf7e6b0f43b2d57fe02a71a
Ritik Sahu <Ritiksahu00980@gmail.com>
2023-05-03 17:56:13 +0530

    this is a assignment 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState } from "react";
+import { Fragment } from "react";
 
-function App() {
+export default function ImageChanger() {
+  const inputRef = useRef();
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    inputRef.current.addEventListener("change", () => {
+      const file = inputRef.current.files[0];
+      const imageUrl = URL.createObjectURL(file);
+      setImageUrl(imageUrl);
+    });
+  }, [inputRef]);
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="image-container">
+        {imageUrl && <img src={"https://images.pexels.com/photos/980384/pexels-photo-980384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} alt="Uploaded" />}
+      </div>
+      <button className="image-button" onClick={handleClick}>
+        {imageUrl ? "Change Image" : "Pick an Image"}
+      </button>
+      <input type="file" ref={inputRef} style={{ display: "none" }} />
+    </Fragment>
   );
 }
-
-export default App;
